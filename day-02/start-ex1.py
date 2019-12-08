1a5db66bb655ea344991a3bece22892f346e2559
Mark Vonk <markjhvonk@gmail.com>
2019-12-08 15:36:07 +0100

    completed day 2 part one

--- day-02/start-ex1.py
+++ day-02/start-ex1.py
@@ -2,33 +2,27 @@ program = open("input.txt", "r")
 
 intcode = list(map(int, program.read().replace('\n', '').split(",")))
 
-section = 12
-prevSection = section - 4
-
-# opCode = intcode[:section]
-
-# for code in opCode:
-#     print(code)
+section = 4
 
 def runCode(opCode):
-    # print(opCode)
+    global intcode
     global section
-    global prevSection
 
     calc = 0
 
     if (opCode[0] == 99):
         print('break')
+        print(intcode[0])
         return
     else:
         if (opCode[0] == 1):
-            calc = opCode[opCode[1]] + opCode[opCode[2]]
+            calc = intcode[opCode[1]] + intcode[opCode[2]]
         elif (opCode[0] == 2):
-            calc = opCode[opCode[1]] * opCode[opCode[2]]
-        print(calc)
+            calc = intcode[opCode[1]] * intcode[opCode[2]]
+        
+        intcode[opCode[3]] = calc
         section += 4
-        print(section)
+        prevSection = section -4
         runCode(intcode[prevSection:section])
 
-# runCode(intcode[prevSection:section])
-print(intcode[prevSection:section])
+runCode(intcode[0:section])
